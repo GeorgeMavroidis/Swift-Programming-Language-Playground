fcbf90559f1470b0526eacd0c4e53d99a8ac9a5b
George Mavroidis <georgeymavroidis@gmail.com>
2014-06-03 12:47:24 -0400

    Generics Update
    Generics

--- Generics.playground/section-1.swift
+++ Generics.playground/section-1.swift
@@ -2,4 +2,36 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+//Write a name inside angle brackets to make a generic function or type
+func repeat<ItemType>(item: ItemType, times: Int) -> ItemType[]{
+    var result = ItemType[]()
+    for i in 0..times{
+        result += item
+    }
+    return result
+}
+repeat("knock", 4)
+
+//You can make generic forms of functions and methods as well as classes enums and structs
+
+//Reimplement the Swift standard librarys optional type
+enum OptionalValue<T>{
+    case None
+    case Some(T)
+}
+var possibleInteger: OptionalValue<Int> = .None
+possibleInteger = .Some(100)
+
+//Use where after the type name to specify a list of requirments  - for example to require the type to implement a protocol, to require two types to be the same, or to require a class to have a particular superclass
+
+func anyCommonElements <T, U where T:Sequence, U:Sequence, T.GeneratorType.Element: Equatable, T.GeneratorType.Element == U.GeneratorType.Element>(lhs:T, rhs:U) ->Bool {
+    for lhsItem in lhs{
+        for rhsItem in rhs{
+            if lhsItem == rhsItem{
+                return true
+            }
+        }
+    }
+    return false
+}
+anyCommonElements([1,2,3], [3])
